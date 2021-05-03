41f0a286d071a72f7630a516d60c7b58c35452a9
Harsh V Singh <harshv.singh@outlook.com>
2021-05-03 17:59:37 +0530

    Added KDE plots by category and a generic function for creating multiplots

--- visualizations.py
+++ visualizations.py
@@ -7,19 +7,21 @@ import seaborn as sns
 
 class Visualizations():
 
-    def __init__(self, fontSize=12, fontWeight="normal", fontFamily="serif"):
+    def __init__(self, palette="viridis", fontSize=12, fontWeight="normal", fontFamily="sans-serif"):
         self.fontSize = fontSize
         self.fontWeight = fontWeight
         self.fontFamily = fontFamily
         
-        self.colorBlue = "#0f4c81"
+        self.snsPalette = palette
         self.colorBlack = "black"
         self.colorGray = "gray"
         self.figFaceColor = "#fafafa"
+        self.figBackgroundColor = "#fafafa"
         
         self.defaultFigSize = (5, 4)
         self.defaultAlpha = 0.8
         self.defaultLineWidth = 1
+        self.wideLineWidth = 1.5
     
     def computeGridSize(self, variables, nrows=None, ncols=None):
         if (nrows is None) & (ncols is None):
@@ -31,21 +33,7 @@ class Visualizations():
             ncols = math.ceil(len(variables) / nrows)
         return (nrows, ncols)
 
-    def drawKDEPlot(self, data, variable, title, figSize=None, showGrid=True, ax=None):       
-        if ax is None:
-            if figSize is None:
-                figSize = self.defaultFigSize
-            fig = plt.figure(figsize=figSize, facecolor=self.figFaceColor, tight_layout=True)
-            ax = fig.add_subplot(1, 1, 1)
-        
-        sns.kdeplot(ax=ax, x=data[variable], color=self.colorBlue, shade=True, linewidth=self.defaultLineWidth, \
-            ec=self.colorBlack, alpha=self.defaultAlpha, zorder=3, legend=False)
-        if showGrid:
-            plt.grid(which="major", axis="x", zorder=0, color=self.colorGray, linestyle=":", dashes=(1,5))
-        plt.xlabel(variable, fontsize=self.fontSize, fontweight=self.fontWeight, fontfamily=self.fontFamily)
-        plt.title(title, fontsize=self.fontSize+2, fontweight=self.fontWeight, fontfamily=self.fontFamily)
-
-    def drawMultipleKDEPlots(self, data, figSize, title, nrows=None, ncols=None, variables=None, showGrid=True):
+    def drawMultiplePlots(self, plotFunction, data, figSize, title, nrows=None, ncols=None, variables=None, **kwargs):
         if variables is None:
             variables = list(data.columns[data.dtypes != object])
         nrows, ncols = self.computeGridSize(variables=variables, nrows=nrows, ncols=ncols)
@@ -54,6 +42,45 @@ class Visualizations():
         fig.suptitle(title, fontsize=self.fontSize+4, fontweight=self.fontWeight, fontfamily=self.fontFamily)
         for i, variable in enumerate(variables):
             locals()["ax" + str(i)] = fig.add_subplot(nrows, ncols, i + 1)
-            self.drawKDEPlot(data=data, variable=variable, title=variable, showGrid=showGrid, ax=locals()["ax" + str(i)])
+            plotFunction(data=data, variable=variable, title=variable, ax=locals()["ax" + str(i)], **kwargs)
 
+    def setPlotProperties(self, g, title, xlabel):
+        g.set_xlabel(xlabel, fontsize=self.fontSize, fontweight=self.fontWeight, fontfamily=self.fontFamily)
+        g.set_title(title, fontsize=self.fontSize+2, fontweight=self.fontWeight, fontfamily=self.fontFamily)
+        g.set_facecolor(self.figBackgroundColor)
+        for s in ["top", "right", "left"]:
+            g.spines[s].set_visible(False)
+
+    def drawKDEPlot(self, data, variable, title, figSize=None, showGrid=True, ax=None):       
+        if ax is None:
+            if figSize is None:
+                figSize = self.defaultFigSize
+            fig = plt.figure(figsize=figSize, facecolor=self.figFaceColor, tight_layout=True)
+            ax = fig.add_subplot(1, 1, 1)
+        g = sns.kdeplot(ax=ax, x=data[variable], palette=self.snsPalette, fill=True, linewidth=self.defaultLineWidth, \
+            ec=self.colorBlack, alpha=self.defaultAlpha, zorder=3, legend=False)
+        self.setPlotProperties(g, title=title, xlabel=variable)
+        if showGrid:
+            plt.grid(which="major", axis="x", zorder=0, color=self.colorGray, linestyle=":", dashes=(1,5))
         
+
+    def drawMultipleKDEPlots(self, data, figSize, title, variables=None, showGrid=True, nrows=None, ncols=None):
+        self.drawMultiplePlots(plotFunction=self.drawKDEPlot, data=data, figSize=figSize, title=title, \
+            nrows=nrows, ncols=ncols, variables=variables, showGrid=showGrid)
+
+    def drawKDEPlotsByCategory(self, data, variable, category, title, figSize=None, showGrid=True, ax=None):       
+        if ax is None:
+            if figSize is None:
+                figSize = self.defaultFigSize
+            fig = plt.figure(figsize=figSize, facecolor=self.figFaceColor, tight_layout=True)
+            ax = fig.add_subplot(1, 1, 1)
+        
+        g = sns.kdeplot(ax=ax, x=data[variable], hue=data[category], palette=self.snsPalette, fill=True, \
+            alpha=self.defaultAlpha, linewidth=self.defaultLineWidth, multiple="stack", zorder=3)
+        self.setPlotProperties(g, title=title, xlabel=variable)
+        if showGrid:
+            plt.grid(which="major", axis="x", zorder=0, color=self.colorGray, linestyle=":", dashes=(1,5))
+
+    def drawMultipleKDEPlotsByCategory(self, data, category, figSize, title, variables=None, showGrid=True, nrows=None, ncols=None):
+        self.drawMultiplePlots(plotFunction=self.drawKDEPlotsByCategory, data=data, figSize=figSize, title=title, \
+            nrows=nrows, ncols=ncols, variables=variables, category=category, showGrid=showGrid)
